3dd8e52cfc06933cbf9035d9d80111b08bde5787
Ken Mondragon <167759990+MrDevsss@users.noreply.github.com>
2026-05-17 15:06:09 +0800

    Update fxmanifest.lua

--- nexus_dmv/fxmanifest.lua
+++ nexus_dmv/fxmanifest.lua
@@ -4,7 +4,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Ken Mondragon'
-description 'DMV Driving School System V2 with Theory Tests and Multiple Vehicles'
+description 'Nexus DMV with Theory Tests and Multiple Vehicles'
 version '2.0.0'
 
 shared_scripts {
